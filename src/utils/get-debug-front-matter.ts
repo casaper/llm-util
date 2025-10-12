b9f64fed2a4d5094de84d4bd60cd7bcaa5dced7e
Casaper <me@casaper.dev>
2025-10-12 13:04:58 +0200

    fix: straighten build and reduce sice with yaml package removal

--- src/utils/get-debug-front-matter.ts
+++ src/utils/get-debug-front-matter.ts
@@ -1,31 +1,32 @@
 import { ChatResponse } from 'ollama';
-import { stringify } from 'yaml';
+
+const createFrontMatter = ({
+  model,
+  eval_count,
+  prompt_eval_count,
+  total_duration,
+  eval_duration,
+  load_duration,
+  prompt_eval_duration,
+  created_at,
+}: ChatResponse) =>
+  [
+    '---',
+    `model: ${model}`,
+    `eval_count: ${eval_count}`,
+    `prompt_eval_count: ${prompt_eval_count}`,
+    `total_duration: ${total_duration}`,
+    `eval_duration: ${eval_duration}`,
+    `load_duration: ${load_duration}`,
+    `prompt_eval_duration: ${prompt_eval_duration}`,
+    `created_at: ${created_at}`,
+    '---',
+    '',
+    '',
+  ].join('\n');
 
 /**
  * If debug is true a Markdown YAML front matter block is returned with some ChatResponse metadata.
  */
-export const getDebugFrontMatter = (
-  debug: boolean,
-  {
-    model,
-    eval_count,
-    prompt_eval_count,
-    total_duration,
-    eval_duration,
-    load_duration,
-    prompt_eval_duration,
-    created_at,
-  }: ChatResponse
-) =>
-  debug
-    ? `---\n${stringify({
-        model,
-        eval_count,
-        prompt_eval_count,
-        eval_duration,
-        load_duration,
-        prompt_eval_duration,
-        total_duration,
-        created_at,
-      })}---\n\n`
-    : '';
+export const getDebugFrontMatter = (debug: boolean, response: ChatResponse) =>
+  debug ? createFrontMatter(response) : '';
