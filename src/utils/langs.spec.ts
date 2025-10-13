432ad60e80b398b5ce3446cc8dfd7152a059a8bc
Casaper <me@casaper.dev>
2025-10-13 10:22:53 +0200

    test(read-md-file): read markdown file functions

--- src/utils/langs.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-
-import { langs } from './langs';
-
-describe('langs', () => {
-  it('includes en and de', () => {
-    expect(langs).toEqual(['en', 'de']);
-  });
-});
